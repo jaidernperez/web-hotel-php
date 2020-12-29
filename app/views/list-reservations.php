9ae87e4eb1857f51a2632abd00525431c69b17ee
jaidernperez <jaider.perez@usantoto.edu.co>
2020-12-29 03:59:47 -0500

    feat: improve admin and list views

--- app/views/list-reservations.php
+++ app/views/list-reservations.php
@@ -42,23 +42,6 @@ if (Session::isValidCredentials()):?>
                             <p class="text-primary m-0 font-weight-bold">Listado de reservaciones</p>
                         </div>
                         <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-6 text-nowrap">
-                                    <div id="dataTable_length" class="dataTables_length" aria-controls="dataTable">
-                                        <label>Show&nbsp;<select
-                                                    class="form-control form-control-sm custom-select custom-select-sm">
-                                                <option value="10" selected="">10</option>
-                                                <option value="25">25</option>
-                                                <option value="50">50</option>
-                                                <option value="100">100</option>
-                                            </select>&nbsp;</label></div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="text-md-right dataTables_filter" id="dataTable_filter"><label><input
-                                                    type="search" class="form-control form-control-sm"
-                                                    aria-controls="dataTable" placeholder="Search"></label></div>
-                                </div>
-                            </div>
                             <div class="table-responsive table mt-2" id="dataTable" role="grid"
                                  aria-describedby="dataTable_info">
                                 <table class="table my-0" id="dataTable" aria-describedby="List reservations">
