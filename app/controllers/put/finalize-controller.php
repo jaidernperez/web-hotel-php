499ca46ee504ff85b87a2e4bdf8ea1b7c9f15195
jaidernperez <jaider.perez@usantoto.edu.co>
2020-12-29 01:35:46 -0500

    feat: add image put controllers

---
+++ app/controllers/put/finalize-controller.php
@@ -0,0 +1,93 @@
+<?php
+
+require_once "../../../config/loader.php";
+
+use App\Controllers\ReservationController;
+use App\Controllers\RoomController;
+use App\Models\ReservationEntity;
+use Config\Session;
+use Utilities\Hash;
+use Utilities\Validate;
+
+$response = [];
+
+if (!Session::isValidCredentials()) {
+    header("Location: ../../views/login.php");
+} else {
+    $reservationId = filter_input(INPUT_GET, "id");
+    $hash = filter_input(INPUT_GET, "sh");
+
+    if (Hash::basicHash($reservationId) != $hash) {
+        $response = [
+            "icon" => "error",
+            "title" => "Error",
+            "msg" => "Acción corrupta"
+        ];
+    } elseif (!isset($reservationId) || empty($reservationId)) {
+        $response = [
+            "icon" => "error",
+            "title" => "Error",
+            "msg" => "No se pudo completar la acción"
+        ];
+    } else {
+
+        $newReservation = new ReservationEntity();
+        $reservationController = new ReservationController();
+        $roomController = new RoomController();
+
+        $reservation = $reservationController->getOneReservation($reservationId);
+        $room = $roomController->getOneRoom($reservation['id_habitacion']);
+
+        try {
+            $endDate = new DateTime(date('d-m-Y'));
+            $startDate = new DateTime($reservation['fecha_inicio']);
+        } catch (Exception $e) {
+        }
+
+        $finalPrice = $reservation['precio_total'];
+
+        $date1 = new DateTime("2015-02-14");
+        $date2 = new DateTime("2015-02-16");
+        $diff = $startDate->diff($endDate)->format('%a');
+
+        if (Validate::isEmpty($finalPrice)) {
+            $finalPrice = (float)$diff * (float)$room['precio'];
+            if ((int)$diff == 0) {
+                $finalPrice = (float)$room['precio'];
+            }
+            $newReservation->setReservationId($reservationId);
+            $newReservation->setRoom($room['id_habitacion']);
+            $newReservation->setPerson($reservation['id_persona']);
+            $newReservation->setStartDate($reservation['fecha_inicio']);
+            $newReservation->setEndDate($endDate->format('Y-m-d'));
+            $newReservation->setFinalPrice($finalPrice);
+            $newReservation->setState(true);
+
+            $roomController->activateAvailabilityRoom($reservation['id_habitacion']);
+
+            $result = $reservationController->updateReservation($newReservation);
+
+            if ($result->rowCount() == 1) {
+                $response = [
+                    "icon" => "success",
+                    "title" => "Éxito",
+                    "msg" => "El precio final de la reservación es {$finalPrice}  por {$diff} días"
+                ];
+            } else {
+                $response = [
+                    "icon" => "error",
+                    "title" => "Error",
+                    "msg" => "No se pudo finalizar la reservacion"
+                ];
+            }
+        } else{
+            $response = [
+                "icon" => "warning",
+                "title" => "Advertencia",
+                "msg" => "La reservación ya está finalizada"
+            ];
+        }
+    }
+}
+Session::setFlashWi("msg", json_encode($response));
+header("Location: ../../views/admin-reservations.php");
---
+++ app/controllers/put/profile-image-controller.php
@@ -0,0 +1,56 @@
+<?php
+
+use App\Controllers\UserController;
+use App\Models\UserEntity;
+use Config\Session;
+use Utilities\Alert;
+use Utilities\UploadImage;
+use Utilities\Validate;
+use Utilities\Validation\ValidateUser;
+
+require_once "../../../config/loader.php";
+
+$response = [];
+if (!Session::isValidCredentials()) {
+    header("Location: ../../views/login.php");
+} else {
+    if (!isset($_FILES["uploadFile"])) {
+        $response["alert"] = Alert::getAlert("error", "Error", "No se pudo leer el archivo");
+    } else {
+        $project = explode('/', $_SERVER['REQUEST_URI'])[1];
+        $folder = "{$_SERVER['DOCUMENT_ROOT']}/{$project}/uploads/images/";
+        $extensions = ["png", "jpg", "jpeg"];
+        $uploadImage = new UploadImage($folder, 500000, $extensions, "img_");
+
+        $result = $uploadImage->uploadFile("uploadFile");
+        if ($result["status"]) {
+            $userController = new UserController();
+            $user = new UserEntity();
+            $user->setUserId(Session::get("user"));
+            $user->setImage($result["name"]);
+            $result = $userController->updateImage($user);
+            if($result->rowCount()==1){
+                $response = [
+                    "icon" => "success",
+                    "title" => "Éxito",
+                    "msg" => "La imágen se ha actualizado con éxito"
+                ];
+            }else{
+                $response = [
+                    "icon" => "error",
+                    "title" => "Error",
+                    "msg" => "La imágen no se ha actualizado"
+                ];
+            }
+        } else {
+            $response = [
+                "icon" => "warning",
+                "title" => "Error",
+                "msg" => $result["error"]
+            ];
+        }
+        Session::setFlashWi("msg", json_encode($response));
+    }
+}
+
+header("Location: ../../views/profile.php");
