7cb36e55fdf746ce2cc303ee89bf17f2c2e46809
jaidernperez <jaider.perez@usantoto.edu.co>
2020-12-29 01:39:19 -0500

    feat: add list views

---
+++ app/views/list-persons.php
@@ -0,0 +1,135 @@
+<?php
+require_once "../../config/loader.php";
+
+use App\Controllers\PersonController;
+use Config\Session;
+
+if (Session::isValidCredentials()):?>
+    <!DOCTYPE html>
+    <html lang="">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <title>List persons</title>
+        <link rel="stylesheet" href="../../public/css/bootstrap.min.css">
+        <link rel="stylesheet"
+              href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+        <link rel="stylesheet" href="../../public/fonts/fontawesome-all.min.css">
+        <link rel="stylesheet" href="../../public/fonts/font-awesome.min.css">
+        <link rel="stylesheet" href="../../public/fonts/fontawesome5-overrides.min.css">
+<link rel="stylesheet" href="../../public/css/animate.min.css">
+    </head>
+
+    <body id="page-top">
+
+    <div id="wrapper">
+        <?php
+        $personController = new PersonController();
+        $persons = $personController->showPersons();
+        ?>
+        <?php require_once "../../public/menu-left.php"; ?>
+
+        <div class="d-flex flex-column" id="content-wrapper">
+            <div id="content">
+                <?php require_once "../../public/header.php"; ?>
+
+                <!-- start list persons content -->
+                <div class="container-fluid">
+                    <h3 class="text-dark mb-4">Personas</h3>
+                    <div class="card shadow">
+                        <div class="card-header py-3">
+                            <p class="text-primary m-0 font-weight-bold">Listado de personas</p>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6 text-nowrap">
+                                    <div id="dataTable_length" class="dataTables_length" aria-controls="dataTable">
+                                        <label>Show&nbsp;<select
+                                                    class="form-control form-control-sm custom-select custom-select-sm">
+                                                <option value="10" selected="">10</option>
+                                                <option value="25">25</option>
+                                                <option value="50">50</option>
+                                                <option value="100">100</option>
+                                            </select>&nbsp;</label></div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="text-md-right dataTables_filter" id="dataTable_filter"><label><input
+                                                    type="search" class="form-control form-control-sm"
+                                                    aria-controls="dataTable" placeholder="Search"></label></div>
+                                </div>
+                            </div>
+                            <div class="table-responsive table mt-2" id="dataTable" role="grid"
+                                 aria-describedby="dataTable_info">
+                                <table class="table my-0" id="dataTable" aria-describedby="List persons">
+                                    <thead>
+                                    <tr>
+                                        <th scope="col">Id</th>
+                                        <th scope="col">Cedula</th>
+                                        <th scope="col">Nombres</th>
+                                        <th scope="col">Apellidos</th>
+                                        <th scope="col">Correo</th>
+                                        <th scope="col">Teléfono</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <?php foreach ($persons as $person): ?>
+                                        <tr>
+                                            <td><?= $person['id_persona'] ?></td>
+                                            <td><?= $person['cedula'] ?></td>
+                                            <td><?= $person['nombres'] ?></td>
+                                            <td><?= $person['apellidos'] ?></td>
+                                            <td><?= $person['correo'] ?></td>
+                                            <td><?= $person['telefono'] ?></td>
+                                        </tr>
+                                    <?php endforeach; ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6 align-self-center">
+                                    <p id="dataTable_info" class="dataTables_info" role="status" aria-live="polite">
+                                        Showing
+                                        1 to 10 of 27</p>
+                                </div>
+                                <div class="col-md-6">
+                                    <nav class="d-lg-flex justify-content-lg-end dataTables_paginate paging_simple_numbers">
+                                        <ul class="pagination">
+                                            <li class="page-item disabled"><a class="page-link" href="#"
+                                                                              aria-label="Previous"><span
+                                                            aria-hidden="true">«</span></a></li>
+                                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                            <li class="page-item"><a class="page-link" href="#"
+                                                                     aria-label="Next"><span
+                                                            aria-hidden="true">»</span></a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end list persons content -->
+
+            </div>
+            <?php require_once "../../public/footer.php"; ?>
+        </div>
+        <a class="border rounded d-inline scroll-to-top" href="#page-top"><span class="fas fa-angle-up"></span></a>
+    </div>
+
+    <script src="../../public/js/jquery.min.js"></script>
+    <script src="../../public/js/bootstrap.min.js"></script>
+    <script src="../../public/js/chart.min.js"></script>
+    <script src="../../public/js/bs-init.js"></script>
+    <script src="../../public/js/jquery.easing.min.js"></script>
+    <script src="../../public/js/theme.js"></script>
+    </body>
+
+    </html>
+<?php
+else:
+    header("Location: ./login.php");
+endif;
+?>
